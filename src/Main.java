a6e9108324e2cb13d47fe8a327240af73ea966dc
Alexandra Vasileva <a.v.arefeva@gmail.com>
2023-04-13 23:05:28 +0300

    Add class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    public static void main(String[] args){
-        int price = 120;
-        int rubPerBonus = 20;
-        int bonus = price / rubPerBonus;
-        System.out.println("Вы получили " + bonus + " бонусных миль за эту покупку");
-
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int cost = 10_000;
+        int miles = service.calculate(cost);
+        System.out.println(miles);
     }
 }
+
